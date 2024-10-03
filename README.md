c275361d7da2ba2cefd8d7b218941bf60474fb15
zacker <iszkmasaki@gmail.com>
2024-10-03 09:52:42 +0900

    feat: カウンター機能を実装

--- README.md
+++ README.md
@@ -6,3 +6,9 @@ cd backend
 pip install -r requirements.txt
 uvicorn main:app --reload
 ```
+
+### フロントエンド
+```sh
+cd frontend
+python -m http.server 3000
+```
--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,57 @@
-from fastapi import FastAPI
+from typing import List
+from logging import getLogger
+from fastapi import FastAPI, WebSocketDisconnect
+from fastapi import FastAPI, WebSocket
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+logger = getLogger(__name__)
 
+counter = 0
+connections: List[WebSocket] = []
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
+@app.websocket("/ws/counter")
+async def websocket_count(websocket: WebSocket):
+    """
+    Request:
+    {
+        "value": 1
+    }
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
+    Response:
+    {
+        "counter": 1
+    }
+    """
+    global counter
+    global connections
+
+    # 新しい接続を待機
+    await websocket.accept()
+    print("Accept new connection")
+
+    # 接続されたクライアントを登録する
+    connections.append(websocket)
+
+    # 現在のカウンターの値を送信
+    await websocket.send_json({"counter": counter})
+
+    try:
+        while True:
+            # クライアントからデータを受信
+            data = await websocket.receive_json()
+            counter += data["value"]
+            print(f"Receive data: {data}")  
+
+            # 接続された全クライアントにデータを送信
+            for connection in connections:
+                await connection.send_json({"counter": counter})
+    except WebSocketDisconnect:
+        # クライアントが切断された場合、接続を削除する
+        print("Disconnect")
+        connections.remove(websocket)
+        # await websocket.close()
